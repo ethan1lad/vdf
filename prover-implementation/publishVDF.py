52a91a66c912184ce8634781d6d0c1108550bffc
Your Name <ethanjgilder@gmail.com>
2022-05-28 19:08:09 +1000

    Support for index encoding

--- prover-implementation/publishVDF.py
+++ prover-implementation/publishVDF.py
@@ -2,7 +2,7 @@ import time
 import requests
 
 from consts import url, headers
-from helpers import nBitRandom, getGaussianPrime, squareRoot, encodeBigInt, encodeCollBigInt
+from helpers import nBitRandom, getGaussianPrime, squareRoot, encodeBigInt, encodeCollBigInt, encodeInt
 
 # Generate prime p
 p = getGaussianPrime(120)
@@ -24,21 +24,26 @@ transaction_to_sign = \
         "inputsRaw": []
     }
 
-for _ in range(25):
+for i in range(25):
     for _ in range(200):
         for _ in range(int(178)):
             n = squareRoot(n,p)
         checkpoints.append(encodeBigInt(n))
     obj_to_append = \
                 {
-                    "address": "3n7QCt34e6s7VwzLXVq3je1HMJQexGLM2vdRifkmVbPu8nVq8G1o8kJGExy8YRajbsmoMX4fdxhg4opmBz3KeS5nCvyCpBP5TP8HxbhiYAMpNmtTwGAzPPefo1cBeF37hSYWVXd1YeWczufwcz6Lq54hmJB57C5hbJjqVRYW4",
+                    "address": "KeFFWP5NAKZ5aiL6N65KZWZMvuN86BzinYvPwpjkqc8xU9FSNNA4gp6bKwBFMhgB3LRkf1jBzyr3uHS9YidBgeb2U4uA7kHrFXUhpDxun29jeHhQSZ1ZsmK6JJeUiXu8sCi86umPukXBXBLGheBWFAPeYaV62AqqjisaN3wWq5PRvM3Qg8e7SHBcBXJMNfAaecPddJxQQHDDr6oUYeNKDp6YvDmpSnaxTeZAJiSomjPEtsVi4DjrgUf114mCRMywMSijZgFBYEZS1Y7KoP5UGCSM4QbEoCvemdq5nK7NymkNT",
                     "value": 3000000,
                     "assets": [
+                        {
+                        "tokenId": "5d1f057b01ee6f6d83683a87da418d31c4d82c548e3eeed1687120f1adbad745",
+                        "amount": 1
+                        }
                     ],
                     "registers": {
                         "R4": encodeCollBigInt(checkpoints),
                         "R5": "06" + encodeBigInt(p),
-                        "R6": "0eb20101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101"
+                        "R6": "0eb20101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101",
+                        "R7": encodeInt(i)
                     }
                 }
     checkpoints = [checkpoints[-1]]
